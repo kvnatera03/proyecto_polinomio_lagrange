8b63bb53b30740712fcb69bb901bb868b22e631f
kvnatera03 <kvnatera03@gmail.com>
2019-11-21 15:00:29 -0400

    polinomio lagrange

--- Kevin_27000066.py
+++ Kevin_27000066.py
@@ -12,22 +12,36 @@ def polinomio_lagrange(X, Y):
     Parámetros:
     X: lista de valores de la variable independiente.
     Y: lista de valores de la variable dependiente.
-    """
+    """ 
 
-     if len(X) != len(Y): raise ValueError("Dimensiones diferentes en X e Y.")
+    if len(X) != len(Y): raise ValueError("Dimensiones diferentes en X e Y.")
 
     # Ordena el par (x, y) en forma ascendente por x.
     pares = list(zip(X, Y))
     pares.sort(key = lambda x: x[0])
     X, Y  = zip(*pares)
 
-     def polinomio(x):
-        pass
-
+    def polinomio(x):
+        lon = len(X)
+        px = 0
+        a = 0
+        while(a < lon):
+            l = Y[a]
+            b = 0
+            while(b < lon):
+                if b != a:
+                    l = l * ((x - X[b]) / (X[a] - X[b]))
+                b += 1 
+            px += l
+            a += 1
+        return px
     return polinomio
 
 
 if __name__ == '__main__':
     # Pruebe aquí su Polinomio de Lagrange ...
-
+    x=[-2,0,2,4]
+    y=[1,-1,3,-2]
+    p=polinomio_lagrange(x, y)
+    print(p(4))
     pass
